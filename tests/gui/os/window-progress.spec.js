ab62f8cc9b0133fa68af84884b7169b5ad4871e9
Shou <x@shou.io>
2017-05-16 13:11:25 +0100

    add tests

--- tests/gui/os/window-progress.spec.js
+++ tests/gui/os/window-progress.spec.js
@@ -1,7 +1,7 @@
 /*
  * Copyright 2016 resin.io
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -19,52 +19,74 @@
 const m = require('mochainon')
 const windowProgress = require('../../../lib/gui/os/window-progress')
 
-describe('Browser: WindowProgress', function () {
-  describe('windowProgress', function () {
-    describe('given a stubbed current window', function () {
-      beforeEach(function () {
+describe('Browser: WindowProgress', function() {
+
+  describe('windowProgress', function() {
+
+    describe('given a stubbed current window', function() {
+
+      beforeEach(function() {
         this.setProgressBarSpy = m.sinon.spy()
+        this.setTitleSpy = m.sinon.spy()
 
         windowProgress.currentWindow = {
-          setProgressBar: this.setProgressBarSpy
+          setProgressBar: this.setProgressBarSpy,
+          setTitle: this.setTitleSpy
         }
       })
 
-      describe('.set()', function () {
-        it('should translate 0-100 percentages to 0-1 ranges', function () {
+      describe('.set()', function() {
+
+        it('should translate 0-100 percentages to 0-1 ranges', function() {
           windowProgress.set(85)
           m.chai.expect(this.setProgressBarSpy).to.have.been.calledWith(0.85)
         })
 
-        it('should set 0 given 0', function () {
+        it('should set 0 given 0', function() {
           windowProgress.set(0)
           m.chai.expect(this.setProgressBarSpy).to.have.been.calledWith(0)
         })
 
-        it('should set 1 given 100', function () {
+        it('should set 1 given 100', function() {
           windowProgress.set(100)
           m.chai.expect(this.setProgressBarSpy).to.have.been.calledWith(1)
         })
 
-        it('should throw if given a percentage higher than 100', function () {
-          m.chai.expect(function () {
+        it('should throw if given a percentage higher than 100', function() {
+          m.chai.expect(function() {
             windowProgress.set(101)
           }).to.throw('Invalid percentage: 101')
         })
 
-        it('should throw if given a percentage less than 0', function () {
-          m.chai.expect(function () {
+        it('should throw if given a percentage less than 0', function() {
+          m.chai.expect(function() {
             windowProgress.set(-1)
           }).to.throw('Invalid percentage: -1')
         })
+
+        it('should set the flashing title', function() {
+          windowProgress.set(100, 'write')
+          m.chai.expect(this.setTitleSpy).to.have.been.calledWith('Etcher – Flashing 100%')
+        })
+
+        it('should set the validating title', function() {
+          windowProgress.set(100, 'check')
+          m.chai.expect(this.setTitleSpy).to.have.been.calledWith('Etcher – Validating 100%')
+        })
+
       })
 
-      describe('.clear()', function () {
-        it('should set -1', function () {
+      describe('.clear()', function() {
+
+        it('should set -1', function() {
           windowProgress.clear()
           m.chai.expect(this.setProgressBarSpy).to.have.been.calledWith(-1)
         })
+
       })
+
     })
+
   })
+
 })
